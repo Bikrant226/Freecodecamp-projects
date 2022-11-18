941eee38d0f862399e5abe5879ba44937c3060af
Bikrant226 <bikrantthakur6@gmail.com>
2022-11-18 19:52:03 +0530

    added timestamp project

---
+++ boilerplate-project-timestamp/index.js
@@ -0,0 +1,67 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+const { application } = require('express');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api',(req,res)=>{
+  res.json({
+    unix:new Date().getTime(),
+    utc:new Date().toUTCString()
+  })
+})
+
+
+
+app.get('/api/:date?',(req,res)=>{
+
+  if(!Date.parse(req.params.date) && !Number(req.params.date)){
+    return res.json({error: "Invalid Date"});
+  }else{
+    let d=new Date(req.params.date)
+    let unixTS,utcTS
+    if(isNaN(req.params.date)){
+      unixTS=d.getTime()
+      utcTS=d.toUTCString()
+    }else{
+      let t=parseInt(req.params.date)
+      console.log()
+      unixTS=t
+      utcTS=new Date(t).toUTCString()
+    }
+    res.json({
+      unix:unixTS,
+      utc:utcTS
+    })
+  }
+  
+})
+
+
+
+
+// listen for requests :)
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
